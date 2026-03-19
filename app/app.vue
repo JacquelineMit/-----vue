df29196b318c6a8cd3070105aff6c85c1cb655e4
jacqueline-vara02 <jacquelinevv@mail.ru>
2026-03-19 10:56:01 +0300

    начало (картинки не добавились)

---
+++ app/app.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <img
+      src=".картинки\потребкорзина.jpg"
+      alt="собранная корзина"
+      class="img"
+    />
+    <img src=".картинки\молоко.jpg" alt="молоко" class="img" />
+  </div>
+  <div>
+    <button class="btn">Добавить в корзину</button>
+    <button class="btn">Добавить в корзину</button>
+    <button class="btn">Добавить в корзину</button>
+  </div>
+</template>
+
+<script></script>
+
+<style>
+.btn {
+  background-color: aliceblue;
+  color: brown;
+  border: 2px solid brown;
+  padding: 10px;
+  margin: 10px;
+}
+.btn::after {
+  background-color: brown;
+  color: aliceblue;
+  border: 2px solid aliceblue;
+}
+
+.img {
+  border: 2px solid brown;
+  margin: 10px;
+}
+</style>
