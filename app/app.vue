bc620fa9ba81f044030a5c25b65e0f2b83614da1
jacqueline-vara02 <jacquelinevv@mail.ru>
2026-03-19 14:44:15 +0300

    декорация страницы (повтор элеменов (убрать надо))

--- app/app.vue
+++ app/app.vue
@@ -1,38 +1,45 @@
 <template>
-  <div>
-    <img
-      src=".картинки\потребкорзина.jpg"
-      alt="собранная корзина"
-      class="img"
-    />
-    <img src=".картинки\молоко.jpg" alt="молоко" class="img" />
-  </div>
-  <div>
-    <button class="btn">Добавить в корзину</button>
-    <button class="btn">Добавить в корзину</button>
-    <button class="btn">Добавить в корзину</button>
-  </div>
+  <body class="main">
+    <header></header>
+    <div>
+      <img
+        src=".картинки\потребкорзина.jpg"
+        alt="собранная корзина"
+        class="img"
+      />
+      <img src=".картинки\молоко.jpg" alt="молоко" class="img" />
+    </div>
+    <div>
+      <button class="btn">Добавить в корзину</button>
+      <button class="btn">Добавить в корзину</button>
+      <button class="btn">Добавить в корзину</button>
+    </div>
+  </body>
 </template>
 
 <script></script>
 
 <style>
+.main {
+  background-color: #c1d3ed;
+}
+
 .btn {
-  background-color: aliceblue;
-  color: brown;
-  border: 2px solid brown;
+  background-color: #c1d3ed;
+  color: #0675bb;
+  border: 2px solid #0675bb;
   padding: 10px;
   margin: 10px;
   border-radius: 20px;
 }
 .btn::after {
-  background-color: brown;
-  color: aliceblue;
-  border: 2px solid aliceblue;
+  background-color: #0675bb;
+  color: #c1d3ed;
+  border: 2px solid #c1d3ed;
 }
 
 .img {
-  border: 2px solid brown;
+  border: 2px solid #0675bb;
   margin: 10px;
 }
 </style>
